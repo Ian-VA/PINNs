16afa14e48d93687c0ee7459b5dd4c9ad1f4b42d
Ian-VA <ianatroshchenko@gmail.com>
2024-01-16 10:57:01 -0800

    Update pinn.py

--- pinn.py
+++ pinn.py
@@ -97,20 +97,6 @@ def make_forward_fn(model: nn.Module, derivative_order: int = 1,) -> list[Callab
 def tuple_to_dict_parameters(
         model: nn.Module, params: tuple[torch.nn.Parameter, ...]
 ) -> OrderedDict[str, torch.nn.Parameter]:
-    """Convert a set of parameters stored as a tuple into a dictionary form
-
-    This conversion is required to be able to call the `functional_call` API which requires
-    parameters in a dictionary form from the results of a functional optimization step which 
-    returns the parameters as a tuple
-
-    Args:
-        model (nn.Module): the model to make the functional calls for. It can be any subclass of
-            a nn.Module
-        params (tuple[Parameter, ...]): the model parameters stored as a tuple
-    
-    Returns:
-        An OrderedDict instance with the parameters stored as an ordered dictionary
-    """
     keys = list(dict(model.named_parameters()).keys())
     values = list(params)
     return OrderedDict(({k:v for k,v in zip(keys, values)}))
